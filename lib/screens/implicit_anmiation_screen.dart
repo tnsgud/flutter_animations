7d987b0ffe08956dfba141cfbcf2133a9d885358
tnsgud <qkrtnsgud0229@gmail.com>
2023-10-03 20:40:58 +0900

    1.2

--- lib/screens/implicit_anmiation_screen.dart
+++ lib/screens/implicit_anmiation_screen.dart
@@ -28,20 +28,18 @@ class _ImplicitAnimationScreenState extends State<ImplicitAnimationScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedAlign(
-              alignment: _visible ? Alignment.topLeft : Alignment.topRight,
+            AnimatedContainer(
               duration: const Duration(milliseconds: 300),
-              child: AnimatedOpacity(
-                opacity: _visible ? 1 : 0.2,
-                duration: const Duration(milliseconds: 300),
-                child: Container(
-                  width: size.width * 0.8,
-                  height: size.width * 0.8,
-                  color: Colors.amber,
-                ),
+              width: size.width * 0.8,
+              height: size.width * 0.8,
+              transform: Matrix4.rotationZ(_visible ? 1 : 0),
+              transformAlignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(_visible ? 100 : 0),
+                color: _visible ? Colors.red : Colors.amber,
               ),
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 50),
             ElevatedButton(
               onPressed: _trigger,
               child: const Text('Go'),
